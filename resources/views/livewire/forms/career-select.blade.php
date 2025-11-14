bec23be3cdfe322244bc0950a26b81630bbaa148
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-11-14 13:36:24 -0400

    delete extra career-select component

--- resources/views/livewire/forms/career-select.blade.php
+++
@@ -1,6 +0,0 @@
-<x-form.select id="career_id" wire:model="career_id">
-    <option value="">Seleccione la carrera...</option>
-    @foreach ($careers as $career)
-        <option value="{{ $career->id }}"> {{ $career->name }}</option>
-    @endforeach
-</x-form.select>
