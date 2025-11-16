4a48a28b453c2c821a14120cc4fe9c863e71e0b4
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2025-11-16 16:27:17 -0400

    fix imagen delay and config vite.config.js for best perfomance.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,15 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.interceptors.response.use(
+    response => response,
+    error => {
+        // Si el token/sesión expiró (401 o 419)
+        if (error.response && (error.response.status === 401 || error.response.status === 419)) {
+            // Redirige al login
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,23 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: ['resources/views/**'],
         }),
     ],
+    server: {
+        host: 'localhost',
+    },
+    watch: {
+        usePolling: false,
+        ignored: [
+            '**/vendor/**',           // 60 MB ignorados
+            '**/node_modules/**',     // 88 MB ignorados
+            '**/storage/**',          // 8 MB ignorados
+            '**/bootstrap/cache/**',
+            '**/public/build/**',
+            '**/database/**',
+            '**/.git/**',
+            '**/tests/**',
+        ]
+    }
 });
